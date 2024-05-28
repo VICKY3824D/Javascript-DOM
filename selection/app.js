65914ec627391c8b48288adac20c83c7969f1a51
Vidky Adhi Pradana <pradana2766@gmail.com>
2024-05-29 06:29:58 +0700

    Belajar Javascript DOM

---
+++ selection/app.js
@@ -0,0 +1,29 @@
+// const allImages = document.getElementsByTagName('img');
+
+// for (let img of allImages) {
+// 	console.log(img.src);
+// 	img.src =
+// 		'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/1920px-Felis_catus-cat_on_snow.jpg';
+// }
+
+// const squareImages = document.getElementsByClassName('square');
+
+// for (let img of squareImages) {
+// 	img.src =
+// 		'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg';
+// }
+
+// const links = document.querySelectorAll('a');
+
+// for (let link of links) {
+// 	link.style.color = "rgb(182, 60, 70)";
+// 	link.style.textDecorationColor = "brown";
+// 	link.style.textDecorationStyle = "double";
+// } 
+
+// const hasatu = document.querySelector('h1');
+
+// const haduwa = document.createElement('h2');
+// haduwa.append('kucing kesukaan kalian semua');
+
+// hasatu.insertAdjacentElement('afterend',haduwa);
